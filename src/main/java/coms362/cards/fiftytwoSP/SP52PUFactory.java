b256fe60187fff1fb3a71b3af69a8eb460c02d66
Jeremy Galang <jjgalang@iastate.edu>
2020-04-12 19:08:44 -0500

    debugging

--- src/main/java/coms362/cards/fiftytwoSP/SP52PUFactory.java
+++ src/main/java/coms362/cards/fiftytwoSP/SP52PUFactory.java
@@ -1,8 +1,17 @@
 package coms362.cards.fiftytwoSP;
 
 import coms362.cards.abstractcomp.GameFactory;
+import coms362.cards.abstractcomp.Player;
+import coms362.cards.abstractcomp.Rules;
+import coms362.cards.abstractcomp.Table;
+import coms362.cards.abstractcomp.View;
+import coms362.cards.abstractcomp.ViewFactory;
+import coms362.cards.fiftytwo.PartyRole;
+import coms362.cards.streams.RemoteTableGateway;
+import model.PlayerFactory;
 
-public class SP52PUFactory implements GameFactory {
+public abstract class SP52PUFactory implements GameFactory, PlayerFactory, ViewFactory {
+
 	
 
 }
