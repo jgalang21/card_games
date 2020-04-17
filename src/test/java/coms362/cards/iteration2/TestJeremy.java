0be0b3a45f640ef750a7b1980ef4637e6a59aa99
Jeremy Galang <jjgalang@iastate.edu>
2020-04-16 19:02:08 -0500

    bit of cleanup

--- src/test/java/coms362/cards/iteration2/TestJeremy.java
+++ src/test/java/coms362/cards/iteration2/TestJeremy.java
@@ -1,37 +1,16 @@
 package coms362.cards.iteration2;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import coms362.cards.abstractcomp.GameFactory;
-import coms362.cards.abstractcomp.Move;
-import coms362.cards.abstractcomp.Player;
 import coms362.cards.abstractcomp.Rules;
-import coms362.cards.abstractcomp.Table;
-import coms362.cards.app.GameController;
 import coms362.cards.app.GameFactoryFactory;
 import coms362.cards.fiftytwo.P52GameFactory;
-import coms362.cards.fiftytwo.PickupPlayer;
 import coms362.cards.fiftytwo.PickupRules;
-import coms362.cards.fiftytwo.SetQuorumCmd;
-import coms362.cards.fiftytwo.TestEndpoint;
-import coms362.cards.fiftytwo.TestGFF;
-import coms362.cards.fiftytwo.TestGameFactory;
 import coms362.cards.fiftytwoSP.PickupRulesSP;
 import coms362.cards.fiftytwoSP.SP52PUFactory;
-import coms362.cards.socket.SocketEvent;
-import coms362.cards.streams.InBoundQueue;
-import coms362.cards.streams.RemoteTableGateway;
-import events.inbound.ConnectEvent;
-import events.inbound.DealEvent;
-import events.inbound.EndPlay;
-import events.inbound.SetQuorumEvent;
-import model.PlayerFactory;
-import model.Quorum;
-import model.TableBase;
 
 /**
  * Test Classes for iteration 2
