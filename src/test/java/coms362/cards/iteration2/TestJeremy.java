63da789e60b66fc858f68b3dc379ab1fe7d10d80
Jeremy Galang <jjgalang@iastate.edu>
2020-04-17 13:30:30 -0500

    added more tests

--- src/test/java/coms362/cards/iteration2/TestJeremy.java
+++ src/test/java/coms362/cards/iteration2/TestJeremy.java
@@ -1,6 +1,8 @@
 package coms362.cards.iteration2;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
@@ -11,6 +13,14 @@ import coms362.cards.fiftytwo.P52GameFactory;
 import coms362.cards.fiftytwo.PickupRules;
 import coms362.cards.fiftytwoSP.PickupRulesSP;
 import coms362.cards.fiftytwoSP.SP52PUFactory;
+import events.remote.AddToPileRemote;
+import events.remote.ShowCardRemote;
+import model.Button;
+import model.Card;
+import model.Deck;
+import model.Location;
+import model.PlayerFactory;
+import model.Quorum;
 
 /**
  * Test Classes for iteration 2
@@ -62,6 +72,65 @@ public class TestJeremy {
 	}
 	
 	
+	//these test are simply the ones I wrote up about in the code coverage analysis
+	
+	@Test
+	public void testJeremy3() {
+		
+		//these test some of the classes in the model package
+		Quorum x = new Quorum(1,1);
+		
+		assertEquals(true, x.meets(1));
+		assertEquals(false, x.exceeds(0)); //it shouldn't have anything yet
+		
+		
+		Location loc = new Location(10,10);
+		
+		Deck deck = new Deck("Deck", loc);
+		
+		assertEquals(10, deck.getLocation().getX());
+		assertEquals(10, deck.getLocation().getY());
+		
+		Button but = new Button("Selector", "Event", "Lablel", loc);
+		
+		
+		but.setEvtName("Name");
+		but.setLabel("Label");
+		but.setLocation(loc);
+		
+		
+		assertEquals("Name", but.getEvtName());
+		assertEquals("Label", but.getLabel());
+		assertEquals(loc, but.getLocation());
+		
+		
+		
+		
+		//these test some of the classes in the events.remote package
+		Card card = new Card();
+		
+		ShowCardRemote rc = new ShowCardRemote(card);
+		
+		//verify that these methods are working as intended, contents may not be accurate
+		assertNotNull(rc.marshall());
+		assertNotNull(rc.stringify());
+		
+		AddToPileRemote apc = new AddToPileRemote("TestName", card);
+		
+		assertNotNull(apc.marshall());
+		assertNotNull(apc.stringify());
+		
+		
+		
+	
+		
+		//one test case that uses the coms362.cards.fiftytwo package
+		P52GameFactory pgf = new P52GameFactory();
+		
+		assertTrue(pgf.createPlayerFactory() instanceof PlayerFactory);
+		
+		
+	}
 	
 
 	
